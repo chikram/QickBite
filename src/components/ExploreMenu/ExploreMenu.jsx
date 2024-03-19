20f1939bbe6fa7008f2b7c6466148a0ae0e52d93
Ikram <ikramaslam1020@gmail.com>
2024-03-19 05:26:02 +0500

    setting active class on menu item list

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { menu_list } from "../../assets/assets";
 import "./Explore.css";
-const ExploreMenu = () => {
+const ExploreMenu = ({ category, setCategory }) => {
   return (
     <div className="container-fluid my-3">
       <h1>Explore Menu</h1>
@@ -9,13 +9,26 @@ const ExploreMenu = () => {
       <div className="d-flex menu-list">
         {menu_list.map((item, index) => {
           return (
-            <div className="p-3 menu-item-list " key={index}>
-              <img src={item.menu_image} alt="" />
+            <div
+              className="p-3 menu-item-list "
+              key={index}
+              onClick={() =>
+                setCategory((prev) =>
+                  prev === item.menu_name ? "All" : item.menu_name
+                )
+              }
+            >
+              <img
+                src={item.menu_image}
+                alt="menu"
+                className={category === item.menu_name ? "active" : ""}
+              />
               <p className="text-center pt-2">{item.menu_name}</p>
             </div>
           );
         })}
       </div>
+      <hr />
     </div>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,15 @@
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 import Header from "../../components/Header/Header";
 import "./Home.css";
+import { useState } from "react";
 import React from "react";
 
 const Home = () => {
+  const [category, setCategory] = useState("All");
   return (
     <div className="container-fluid my-3">
       <Header />
-      <ExploreMenu />
+      <ExploreMenu category={category} setCategory={setCategory} />
     </div>
   );
 };
