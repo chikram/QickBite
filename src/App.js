82f596b4c2416aa95a8e1119e52ed3ed16ac14fc
Ikram <ikramaslam1020@gmail.com>
2024-04-24 01:30:08 +0500

    create place order feature and stripe payment integration

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Footer from "./components/Footer/Footer";
 import { useState } from "react";
 import Login from "./components/LoginModal/Login";
+import Verify from "./pages/Verify/Verify";
 function App() {
   const [showLogin, setShowLogin] = useState(false);
 
@@ -19,6 +20,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/order" element={<PlaceOrder />} />
+          <Route path="/verify" element={<Verify />} />
 
         </Routes>
         <Footer />
--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,113 +1,195 @@
+import axios from "axios";
 import { StoreContext } from "../../context/StoreContext";
 import "./PlaceOrder.css";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 const PlaceOrder = () => {
-  const { getTotalCartAmount } = useContext(StoreContext);
+  const { getTotalCartAmount, token, food_list, cartItems, url } =
+    useContext(StoreContext);
+
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    street: "",
+    city: "",
+    state: "",
+    zipcode: "",
+    country: "",
+    phone: "",
+  });
+
+  const onchangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData((data) => ({ ...data, [name]: value }));
+  };
+
+  const placeOrder = async (event) => {
+    event.preventDefault();
+    let orderItems = [];
+    food_list.map((item) => {
+      if (cartItems[item._id] > 0) {
+        let itemInfo = item;
+        itemInfo["quantity"] = cartItems[item._id];
+        orderItems.push(itemInfo);
+      }
+    });
+    let orderData = {
+      address: data,
+      items: orderItems,
+      amount: getTotalCartAmount() + 2,
+    };
+    let response = await axios.post(url + "/api/order/place", orderData, {
+      headers: { token },
+    });
+    if (response.data.success) {
+      const { session_url } = response.data;
+      window.location.replace(session_url);
+    } else {
+      alert("Error");
+    }
+  };
+
   return (
     <>
-      <div className="row place-order">
-        <div className="col mb-3">
-          <h4>Delivery Information</h4>
-          <form>
+      <form onSubmit={placeOrder}>
+        <div className="row place-order">
+          <div className=" col-xs-12 mb-3 col-sm-6 col-lg-6 col-md-6">
+            <h4>Delivery Information</h4>
+
             <div className="mb-3 d-flex">
               <input
+                name="firstName"
+                onChange={onchangeHandler}
+                value={data.firstName}
                 type="text"
                 className="form-control"
-                id="name"
                 aria-describedby="emailHelp"
                 placeholder="First name"
+                required
               />
               <input
+                name="lastName"
+                onChange={onchangeHandler}
+                value={data.lastName}
                 type="text"
                 className="form-control ms-2"
-                id="name"
                 aria-describedby="emailHelp"
                 placeholder="First name"
+                required
               />
             </div>
             <div className="mb-3">
               <input
-                class="form-control"
+                name="email"
+                onChange={onchangeHandler}
+                value={data.email}
+                className="form-control"
                 type="text"
                 placeholder="Email Adress"
                 aria-label="default input example"
+                required
               />
             </div>
             <div className="mb-3">
               <input
-                class="form-control"
+                name="street"
+                onChange={onchangeHandler}
+                value={data.street}
+                className="form-control"
                 type="text"
                 placeholder="Street"
                 aria-label="default input example"
+                required
               />
             </div>
             <div className="mb-3 d-flex">
               <input
+                name="city"
+                onChange={onchangeHandler}
+                value={data.city}
                 type="text"
                 className="form-control"
                 aria-label="default input example"
                 placeholder="City"
+                required
               />
               <input
+                name="state"
+                onChange={onchangeHandler}
+                value={data.state}
                 type="text"
                 className="form-control ms-2"
                 aria-label="default input example"
                 placeholder="State"
+                required
               />
             </div>
             <div className="mb-3 d-flex">
               <input
+                name="country"
+                onChange={onchangeHandler}
+                value={data.country}
                 type="text"
                 className="form-control"
                 aria-label="default input example"
                 placeholder="Country"
+                required
               />
               <input
+                name="zipcode"
+                onChange={onchangeHandler}
+                value={data.zipcode}
                 type="text"
                 className="form-control ms-2"
-                id="Zip Code"
                 aria-label="default input example"
                 placeholder="Zip Code"
+                required
               />
             </div>
             <div className="mb-3">
               <input
-                class="form-control"
+                name="phone"
+                onChange={onchangeHandler}
+                value={data.phone}
+                className="form-control"
                 type="text"
                 placeholder="Phone"
                 aria-label="default input example"
+                required
               />
             </div>
-          </form>
-        </div>
-        <div className="col-6 w-50 my-2">
-          <h4>Cart Totals</h4>
-          <div className="card mb-5">
-            <div className="card-body">
-              <div className="d-flex border-bottom">
-                <p className="card-text ">Subtotals</p>
-                <p style={{ paddingLeft: "6%" }}> ${getTotalCartAmount()}</p>
+          </div>
+          <div className="col-sm-6 col-lg-4 col-md-4 w-40 my-2 col-xm-6">
+            <h4>Cart Totals</h4>
+            <div className="card mb-5">
+              <div className="card-body">
+                <div className="d-flex border-bottom">
+                  <p className="card-text ">Subtotals</p>
+                  <p style={{ paddingLeft: "6%" }}> ${getTotalCartAmount()}</p>
+                </div>
+                <div className="d-flex border-bottom">
+                  <p className="card-text">Delivery Fee</p>
+                  <p style={{ paddingLeft: "2%" }}>
+                    ${getTotalCartAmount() === 0 ? 0 : 5}
+                  </p>
+                </div>
+                <div className="d-flex  border-bottom">
+                  <p className="card-text fw-bold">Total</p>
+                  <p style={{ paddingLeft: "10%" }}>
+                    $ $
+                    {getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 5}
+                  </p>
+                </div>
+                <button type="submit" className="btn btn-danger my-2 btn-total">
+                  PROCEED TO PAYMENT
+                </button>
               </div>
-              <div className="d-flex border-bottom">
-                <p className="card-text">Delivery Fee</p>
-                <p style={{ paddingLeft: "2%" }}>
-                  ${getTotalCartAmount() === 0 ? 0 : 5}
-                </p>
-              </div>
-              <div className="d-flex  border-bottom">
-                <p className="card-text fw-bold">Total</p>
-                <p style={{ paddingLeft: "10%" }}>
-                  $ ${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 5}
-                </p>
-              </div>
-              <button className="btn btn-danger my-2 btn-total">
-                PROCEED TO PAYMENT
-              </button>
             </div>
           </div>
         </div>
-      </div>
+      </form>
     </>
   );
 };
